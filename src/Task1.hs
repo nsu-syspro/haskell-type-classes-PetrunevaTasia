34c5aa8daa04f1318933b7bd74a050e2b11a9997
PetrunevaTasia <hext2004t@gmail.com>
2025-04-06 22:42:37 +0700

    tasks completed

--- src/Task1.hs
+++ src/Task1.hs
@@ -28,7 +28,9 @@ data IExpr =
 -- 9
 --
 evalIExpr :: IExpr -> Integer
-evalIExpr = error "TODO: define evalIExpr"
+evalIExpr (Lit a)   = a
+evalIExpr (Add a b) = evalIExpr a + evalIExpr b
+evalIExpr (Mul a b) = evalIExpr a * evalIExpr b
 
 -- * Parsing
 
@@ -37,6 +39,14 @@ class Parse a where
   -- | Parses value 'a' from given string
   -- wrapped in 'Maybe' with 'Nothing' indicating failure to parse
   parse :: String -> Maybe a
+
+instance Parse Integer where
+  parse x = if num x then Just(read x) else Nothing
+
+instance Parse Bool where
+  parse "1" = Just True
+  parse "0" = Just False
+  parse _ = Nothing
 
 -- | Parses given expression in Reverse Polish Notation
 -- wrapped in 'Maybe' with 'Nothing' indicating failure to parse
@@ -55,7 +65,28 @@ class Parse a where
 -- Nothing
 --
 instance Parse IExpr where
-  parse = error "TODO: define parse (Parse IExpr)"
+  parse [] = Nothing
+  parse list = parseR (words list) []
+
+parseR :: [String] -> [IExpr] -> Maybe IExpr
+parseR [] [expr] = Just expr
+parseR [] _ = Nothing
+parseR (x : xs) stack = if num x then parseR xs (Lit (read x) : stack) else 
+  case x of
+    "+" -> case stack of
+      (a : b : bs) -> parseR xs (Add b a : bs)
+      _ -> Nothing
+    "*" -> case stack of
+      (a : b : bs) -> parseR xs (Mul b a : bs)
+      _ -> Nothing
+    _ -> Nothing
+
+
+num :: String -> Bool
+num = all isDigit 
+
+isDigit :: Char -> Bool
+isDigit c = c `elem` ['0'..'9']
 
 -- * Evaluation with parsing
 
@@ -77,4 +108,6 @@ instance Parse IExpr where
 -- Nothing
 --
 evaluateIExpr :: String -> Maybe Integer
-evaluateIExpr = error "TODO: define evaluateIExpr"
+evaluateIExpr str = case parse str of
+  Just a  -> Just (evalIExpr a)
+  Nothing -> Nothing  
--- src/Task3.hs
+++ src/Task3.hs
@@ -1,7 +1,14 @@
 {-# OPTIONS_GHC -Wall #-}
 -- The above pragma enables all warnings
 
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 module Task3 where
+
+import Task1 (Parse, Parse(..))
+import Task2 (Reify, Eval, Eval(..), evaluate)
+import Data.List (nub)
+import Data.Maybe (isNothing, fromMaybe)
 
 -- | Solves SAT problem for given boolean formula written in Reverse Polish Notation
 --
@@ -15,6 +22,21 @@ module Task3 where
 --
 -- All other words are considered as variables.
 --
+
+data BoolOp = And | Xor | Or
+  deriving Show
+
+instance Parse BoolOp where
+  parse "and" = Just And
+  parse "xor" = Just Xor
+  parse "or"  = Just Or
+  parse _     = Nothing
+
+instance Eval Bool BoolOp where
+  evalBinOp And a b = a && b
+  evalBinOp Xor a b = a /= b
+  evalBinOp Or  a b = a || b
+
 -- Usage example:
 --
 -- >>> solveSAT "x y and"
@@ -27,4 +49,28 @@ module Task3 where
 -- Nothing
 --
 solveSAT :: String -> Maybe Bool
-solveSAT = error "TODO: define solveSAT"
+solveSAT str = 
+    if any isNothing results then Nothing
+    else Just satisfiable
+    where 
+        envs = createListOfLists str
+        results = map (`evalLExpr` str) envs    
+        boolResults = map (fromMaybe False) results
+        satisfiable = or boolResults 
+
+
+createListOfLists :: String -> [[(String, Bool)]]
+createListOfLists str = mapM (\var -> [(var, False), (var, True)]) listUniqueWords
+    where listUniqueWords = wordsNotInList ["and", "or", "xor"] (words str)
+        
+
+wordsNotInList :: [String] -> [String] -> [String]
+wordsNotInList blacklist list =
+  filter (`notElem` blacklist) (nub list)
+
+
+evalLExpr :: [(String, Bool)] -> String -> Maybe Bool
+evalLExpr = evaluate reifyBool
+
+reifyBool :: Reify Bool BoolOp
+reifyBool = id
